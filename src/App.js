e7a285e1c124c216c27fedecd37cc67376131f65
Anacxx <anacarolina.clcx@gmail.com>
2023-05-08 15:17:33 -0300

    dsa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import styled, {createGlobalStyle} from 'styled-components'
+import React, { useState } from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
 import PokemonCard from './Components/PokemonCard/PokemonCard';
 
 const GlobalStyles = createGlobalStyle`
@@ -14,10 +14,8 @@ const FlexContainer = styled.div`
   flex-wrap: wrap;
 `
 function App() {
-  
 
-  // Para fazer seus próximos pokemons, crie novos estados ultilizando como referência o objeto abaixo:
-  /*{
+  const [pokemon, setPokemon] = useState({
     name: "Pichu",
     type: "Electric",
     evolved: false,
@@ -25,19 +23,53 @@ function App() {
     color: 'yellow',
     image: 'https://archives.bulbagarden.net/media/upload/archive/f/f3/20130810070434%210172Pichu.png',
     id: 0
-  }
-  */
-  
+  })
+  const evolucaoPichu = {
+    name: "pikachu",
+    type: "Electric",
+    evolved: true,
+    weight: 2,
+    color: 'yellow',
+    image: 'https://archives.bulbagarden.net/media/upload/4/4a/0025Pikachu.png',
+    id: 0
 
-  return ( <>
-  <GlobalStyles/>
+  }
+  const [munchlax, setMunchlax] = useState({
+    name: "Munchlax",
+    type: "Normal",
+    evolved: false,
+    weight: 105,
+    color: 'blue',
+    image: 'https://archives.bulbagarden.net/media/upload/thumb/5/57/0446Munchlax.png/250px-0446Munchlax.png',
+    id: 0
+  })
+  const evolucaoMunchlax = {
+    name: "Snorlax",
+    type: "Normal",
+    evolved: true,
+    weight: 460,
+    color: '#A7A877',
+    image: 'https://archives.bulbagarden.net/media/upload/thumb/3/3f/0143Snorlax.png/250px-0143Snorlax.png',
+    id: 0
+  }
+  return (<>
+    <GlobalStyles />
     <FlexContainer>
       {/* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */}
-      <PokemonCard/>
+      <PokemonCard
+        pokemon={pokemon}
+        evolucaoPichu={evolucaoPichu}
+        setPokemon={setPokemon}
+      />
+      <PokemonCard
+        pokemon={munchlax}
+        evolucaoPichu={evolucaoMunchlax}
+        setPokemon={setMunchlax}
+      />
       {/* Crie aqui seus próximos pokemons! */}
     </FlexContainer>
   </>
-    
+// MUITO OBRIGADAAAAA BRRRRRRRRRRRRRRRRRRRRRR <3
   );
 }
 
--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import {Card, PokemonName, PokemonType, EvolveButton} from './styles'
+//
 
 
+const PokemonCard = ({pokemon, evolucaoPichu, setPokemon}) => {
 
-const PokemonCard = (props) => {
-
-    const evoluirPokemon = () => {
-        console.log("Cliquei no botão de evoluir")
+    const evoluirPokemon = (novoPokemon) => {
+      setPokemon(novoPokemon)
     }
     
   return (
-    <Card color={}>
-        <img src={} alt={`Pokemon`}/>
-        <PokemonName>{}</PokemonName>
-        <PokemonType>{}</PokemonType>
-        <p>{}kg</p>
+    <Card color={pokemon.color}>
+        <img src={pokemon.image} alt={`Pokemon`} width={100}/>
+        <PokemonName>{pokemon.name}</PokemonName>
+        <PokemonType>{pokemon.type}</PokemonType>
+        <p>{pokemon.weight}kg</p>
 
-        <EvolveButton onClick={() => evoluirPokemon()}>Evoluir!</EvolveButton>
+        <EvolveButton onClick={() => evoluirPokemon(evolucaoPichu)}>Evoluir!</EvolveButton>
     </Card>
   )
 }
